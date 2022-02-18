fbd9b4700ce19836e582ae2800ff23c890c8d819
Tatyana Sinichenkova <t.sinichenkova@infinnity.ru>
2022-02-18 18:59:46 +0500

    fix

--- src/test/java/org/web/ui/lesson5/TestBase.java
+++ src/test/java/org/web/ui/lesson5/TestBase.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestBase {
+public abstract class TestBase {
 
     private static WebDriver driver;
 
